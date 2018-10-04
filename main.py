c233f9528095c6f53dfb692222d1e4028b12b7de
mikanbox <popoloas@yahoo.co.jp>
2018-10-04 15:43:04 +0900

    add test words

--- main.py
+++ main.py
@@ -356,21 +356,23 @@ def mainRoutine(event=0,time=0,pref='大阪'):
             if (j.name == i.name):
                 continue
 
-            if (db.session.query(SpotDist).filter(SpotDist.id_from == i.id).filter(SpotDist.id_to == j.id).count() > 0 ):
-                r = db.session.query(SpotDist).filter(SpotDist.id_from == i.id).filter(SpotDist.id_to == j.id)
-                for r_elements in r:
-                    Journey.timeEdge[i.name,j.name] = r_elements.time
-                    Journey.timeEdge[j.name,i.name] = r_elements.time
-                    print("Data is discovered")
-                    
-                continue
+            # if (db.session.query(SpotDist).filter(SpotDist.id_from == i.id).filter(SpotDist.id_to == j.id).count() > 0 ):
+            #     r = db.session.query(SpotDist).filter(SpotDist.id_from == i.id).filter(SpotDist.id_to == j.id)
+            #     for r_elements in r:
+            #         Journey.timeEdge[i.name,j.name] = r_elements.time
+            #         Journey.timeEdge[j.name,i.name] = r_elements.time
+            #         print("Data is discovered")
 
-            if (db.session.query(SpotDist).filter(SpotDist.id_from == j.id).filter(SpotDist.id_to == i.id).count() > 0 ):
-                r = db.session.query(SpotDist).filter(SpotDist.id_from == i.id).filter(SpotDist.id_to == j.id)
-                Journey.timeEdge[i.name,j.name] = r.time
-                Journey.timeEdge[j.name,i.name] = r.time
-                print("Data is discovered")
-                continue
+            #     continue
+
+            # if (db.session.query(SpotDist).filter(SpotDist.id_from == j.id).filter(SpotDist.id_to == i.id).count() > 0 ):
+            #     r = db.session.query(SpotDist).filter(SpotDist.id_from == i.id).filter(SpotDist.id_to == j.id)
+            #     Journey.timeEdge[i.name,j.name] = r.time
+            #     Journey.timeEdge[j.name,i.name] = r.time
+            #     print("Data is discovered")
+            #     continue
+
+                
                 
             # i-jパスがない時
             spotdist = SpotDist()
