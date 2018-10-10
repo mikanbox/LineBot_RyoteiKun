040bbd9298258d145355751f5ae454799698c1d7
mikanbox <popoloas@yahoo.co.jp>
2018-10-10 11:55:06 +0900

    modify lp

---
+++ APIkey.py
@@ -0,0 +1,9 @@
+global googleAPIkey
+# googleAPIkey = "AIzaSyD9PKwDNyYQep3mw2M_cwUmWU3Kl9iNhRM"
+
+global channel_secret
+# channel_secret = "c9f4a586a8d8b03ce5f6008e79d1414e"
+global channel_access_token
+# channel_access_token = "iGl4or4uXfRGwd9w0dk8UoDMDWn4z7KGtjSMC67WkwzJERiD+FLHwIkEhLeRCXLwuSg4MVuIvoVCDoxPjsJ9azXNe5MTPhPTWwBpf8e+1uuSW/FCL38Naqb0hehsaIqdoDApewB07WrdMuIR0bWMvQdB04t89/1O/w1cDnyilFU="
+
+times = int(os.environ.get('TIMES',3))
