e942b9b20f536b489c63d7765546eac063fdf4c7
mikanbox <popoloas@yahoo.co.jp>
2018-10-10 13:15:38 +0900

    fix regex

--- regexfunc.py
+++ regexfunc.py
@@ -3,18 +3,20 @@ import re
 tt_ksuji = str.maketrans('一二三四五六七八九〇壱弐参', '1234567890123')
 
 
+Pref_List = ['北海道','青森','岩手','宮城','秋田','山形','福島','栃木','群馬','茨城','埼玉','千葉','東京','神奈川','山梨','長野','新潟','富山','石川','福井','静岡','岐阜','愛知','三重','滋賀','京都','大阪','兵庫','奈良','和歌山','鳥取','島根','岡山','広島','山口','徳島','香川','愛媛','高知','福岡','佐賀','長崎','熊本','大分','宮崎','鹿児島','沖縄']
 
 
 # -------------------------------------------
 # regexによる言語処理
 # -------------------------------------------
+#計画開始
 def getJourney(text):
     pattern = r".*旅行.*"
     match = re.search(pattern, text)
     if not match:
         return False
     return True
-
+#県を指定
 def getPref(text):
     for p in Pref_List:
         # rをつけるとエスケープシーケンスが無向に
@@ -55,8 +57,7 @@ def getSumTime(text):
         return starttime,endtime
     return False
 
-
-
+#やめる
 def getStop(text):
     for p in ['やめる','やめた','終了','止める','止めた']:
         pattern = r".*" + p + r".*"
@@ -64,7 +65,7 @@ def getStop(text):
         if match:
             return True #テキスト(県名)を返す
     return False
-
+#ヘルプ
 def getHelp(text):
     for p in ['HELP','ヘルプ','へるぷ','Help','help']:
         pattern = r".*" + p + r".*"
@@ -72,7 +73,7 @@ def getHelp(text):
         if match:
             return True #テキスト(県名)を返す
     return False
-
+#スポット登録
 def getSpot(text):
     for p in ['登録','とうろく']:
         pattern = r".*" + p + r".*"
