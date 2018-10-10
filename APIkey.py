23d3a8bb5931eb1cff55f49d773cf373cc23c6b6
mikanbox <popoloas@yahoo.co.jp>
2018-10-10 11:58:39 +0900

    modify lp

--- APIkey.py
+++ APIkey.py
@@ -1,12 +1,7 @@
 import os
 global googleAPIkey
-# googleAPIkey = "AIzaSyD9PKwDNyYQep3mw2M_cwUmWU3Kl9iNhRM"
 googleAPIkey = os.environ["googleAPIkey"]
 global channel_secret
-# channel_secret = "c9f4a586a8d8b03ce5f6008e79d1414e"
-
 channel_secret = os.environ["channel_secret"]
 global channel_access_token
-# channel_access_token = "iGl4or4uXfRGwd9w0dk8UoDMDWn4z7KGtjSMC67WkwzJERiD+FLHwIkEhLeRCXLwuSg4MVuIvoVCDoxPjsJ9azXNe5MTPhPTWwBpf8e+1uuSW/FCL38Naqb0hehsaIqdoDApewB07WrdMuIR0bWMvQdB04t89/1O/w1cDnyilFU="
-
 channel_access_token = os.environ["channel_access_token"]
